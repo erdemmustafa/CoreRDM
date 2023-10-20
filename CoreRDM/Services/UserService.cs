83a91a56300084118dba321b4325cf6d541cb933
Mustafa Erdem <_Mustafa.Erdem.AZ@sodexo.com>
2023-10-20 23:32:31 +0300

    commit

--- CoreRDM/Services/UserService.cs
+++ CoreRDM/Services/UserService.cs
@@ -19,7 +19,7 @@ namespace CoreRDM.Services
     {// users hardcoded for simplicity, store in a db with hashed passwords in production applications
         private List<User> _users = new List<User>
     {
-        new User { Id = 1, FirstName = "Test", LastName = "User", Username = "test", Password = "test" }
+        new User { Id = 1, FirstName = "Mustafa", LastName = "Erdem", Username = "test", Password = "test" }
     };
 
         private readonly AppSettings _appSettings;
